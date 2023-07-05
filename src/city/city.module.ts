6e3afc1b0688562ddf1b9937d034c89b7395d388
Gustavo Almeida <gustavo_80b@hotmail.com>
2023-07-05 17:55:19 -0300

    feat: new features state, city and address migrations and cache

---
+++ src/city/city.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './entities/city.entity';
+import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
+import { CacheModule } from 'src/cache/cache.module';
+
+@Module({
+  imports: [
+    CacheModuleNest.register({
+      ttl: 1000000000000,
+    }),
+    TypeOrmModule.forFeature([CityEntity]),
+    CacheModule,
+  ],
+  controllers: [CityController],
+  providers: [CityService],
+})
+export class CityModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/createUser.dto';
-import { UserEntity } from './interfaces/user.entity';
+import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -17,6 +17,7 @@ export class UserService {
 
     return this.usersRepository.save({
       ...createUserDTO,
+      type_user: 0,
       password: hashPassword,
     });
   }
